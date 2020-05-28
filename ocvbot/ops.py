02edcd71a7215dfc7becdbcb2d1b5541ce32f1ec
takelley1 <hxyz@protonmail.com>
2020-05-27 21:50:47 -0400

    Super rough initial implementation of a GPS-like system
    The "OSRS Positioning System" (OPS) template-matches the
    minmap to a region of the world map to determine where the
    player is. If it's given a set of "destination" coordinates,
    it can figure out how far away those coordinates are from the
    player's current position and move there. This was inspired by
    Olly's "RSWalk" for Simba.
    
    Signed-off-by: takelley1 <hxyz@protonmail.com>

---
+++ ocvbot/ops.py
@@ -0,0 +1,78 @@
+# The OSRS Positioning System
+
+import logging as log
+import yaml
+
+import pyautogui
+from ocvbot import behavior, input
+from ocvbot import DISPLAY_WIDTH, DISPLAY_HEIGHT, \
+    skilling, behavior, vision
+vision.init_vision()
+
+import os
+
+def main():
+    os.chdir('/home/austin/ocvbot/ocvbot')
+    # Find chunk within world
+    #chunk = pyautogui.locate('./world3-6.png',
+                             #'./world.png', confidence=0.9)
+    # Break up chunk tuple
+    #(chunk_left, chunk_top, chunk_width, chunk_height) = chunk
+    chunk_left = 4608
+    chunk_top = 2304
+    chunk_width = 768
+    chunk_height = 768
+
+    # Find needle within chunk
+    needle = pyautogui.locate('./minimap-slice.png',
+                              './world3-6.png', confidence=0.7)
+    print("needle is", needle)
+    (needle_chunk_left, needle_chunk_top, needle_width, needle_height) = needle
+
+    # Get needle coordinates relative to world
+    needle_world_left = needle_chunk_left + chunk_left
+    needle_world_top = needle_chunk_top + chunk_top
+
+    # Get center of needle within world
+    needle_world_center_x = int((needle_world_left + needle_width) - (needle_width / 2))
+    needle_world_center_y = int((needle_world_top + needle_height) - (needle_height / 2))
+
+    # Get center of needle within client window
+    from ocvbot.vision import vclient
+    print("vclient is", vclient)
+    needle_client_center = vclient.wait_for_image(loctype='center', needle='/home/austin/ocvbot/ocvbot/minimap-slice.png', loop_num=1)
+    (needle_client_center_x, needle_client_center_y) = needle_client_center
+
+    # Get destination
+    dest_world_center_x = 4986
+    dest_world_center_y = 2892
+
+    # Figure out how far the destination is from the current location
+    dest_distance_x = dest_world_center_x - needle_world_center_x
+    dest_distance_y = dest_world_center_y - needle_world_center_y
+
+    # If the destination distance is larger than the size of the minimap,
+    #   reduce the current click distance to the edge of the minimap
+    if dest_distance_x >= 30:
+        log.info("dest distance X is over value")
+        click_pos_x = needle_client_center_x + 30
+    elif dest_distance_x >= (-30):
+        log.info("dest distance X is under value")
+        click_pos_x = needle_client_center_x - 30
+    else:
+        click_pos_x = needle_client_center_x + dest_distance_x
+
+    if dest_distance_y >= 30:
+        log.info("dest distance Y is over value")
+        click_pos_y = needle_client_center_y + 30
+    elif dest_distance_x >= (-30):
+        log.info("dest distance Y is under value")
+        click_pos_y = needle_client_center_y - 30
+    else:
+        click_pos_y = needle_client_center_y + dest_distance_y
+
+    # Now, make the click
+    input.click_coord(click_pos_x, click_pos_y)
+
+if __name__ == '__main__':
+    main()
