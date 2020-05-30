e127e8a9b29df9318a676e1012a5e9b63b0bc43e
takelley1 <hxyz@protonmail.com>
2020-05-30 14:20:17 -0400

    Fix OS command in __init__ removing leftover screenshots
    If the script is stopped with a CTRL-c, sometimes leftover
    PyAutoGUI screenshots will appear in the working directory.
    This command removes them before the script starts.
    
    Signed-off-by: takelley1 <hxyz@protonmail.com>

--- ocvbot/__init__.py
+++ ocvbot/__init__.py
@@ -9,15 +9,16 @@ sys.setrecursionlimit(9999)
 log.basicConfig(format='%(asctime)s %(filename)s.%(funcName)s - %(message)s'
                 , level='INFO')
 
-# TODO: Find a better way to do this.
-# Clean up left over screenshots from failed runs.
-# sub.Popen(["rm", "./.screenshot2*"])
-
-# Make sure the program's working directory is the directory that this
-#   file is located in.
+# Make sure the program's working directory is the directory in which
+#   this file is located.
 absolute_path = os.path.abspath(__file__)
 dir_name = os.path.dirname(absolute_path)
 os.chdir(dir_name)
+
+# TODO: Find a better way to do this.
+# TODO: Add a check to only run this if the OS is Linux.
+# Clean up left over screenshots from previous runs.
+os.system('rm .screenshot2*.png >/dev/null 2>&1')
 
 
 def killer():
