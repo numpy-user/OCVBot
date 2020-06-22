b5ec187471b46e24d056bb92ab30b9b1063bbfb6
takelley1 <hxyz@protonmail.com>
2020-06-21 23:32:23 -0400

    Update formatting and docstrings in input.py
    Signed-off-by: takelley1 <hxyz@protonmail.com>

--- ocvbot/input.py
+++ ocvbot/input.py
@@ -20,23 +20,23 @@ class Mouse:
     Class to move and click the mouse cursor.
 
     Args:
-        ltwh (tuple): A 4-member tuple containing the left, top, width,
-                      and height coordinates. The width and height
-                      values are used for randomizing the location of
-                      the mouse cursor.
-        sleep_range (tuple): A 4-member tuple containing the minimum
-                             and maximum number of miliseconds to wait
-                             before performing the action, and the
-                             minimum and maximum number of miliseconds
-                             to wait after performing the action,
-                             default is (0, 500, 0, 500).
-        action_duration_range (tuple): A 2-member tuple containing the
+        ltwh (tuple): A 4-tuple containing the left, top, width, and
+                      height coordinates. The width and height values
+                      are used for randomizing the location of the mouse
+                      cursor.
+        sleep_range (tuple): A 4-tuple containing the minimum and maximum
+                             number of miliseconds to wait before
+                             performing the action, and the minimum and
+                             maximum number of miliseconds to wait after
+                             performing the action, default is
+                             (0, 500, 0, 500).
+        action_duration_range (tuple): A 2-tuple containing the
                                        minimum and maximum number of
                                        miliseconds during which the
                                        action will be performed, such as
                                        holding down the mouse button,
                                        default is (1, 100).
-        move_duration_range (tuple): A 2-member tuple containing the
+        move_duration_range (tuple): A 2-tuple containing the
                                      minimum and maximum number of
                                      miliseconds to take to move the
                                      mouse cursor to its destination,
@@ -44,13 +44,13 @@ class Mouse:
         button (str): The mouse button to click with, default is left.
 
     """
-
     def __init__(self,
                  ltwh,  # "left, top, width, height"
                  sleep_range=(0, 500, 0, 500),
                  move_duration_range=(50, 1500),
                  action_duration_range=(1, 100),
                  button='left'):
+
         self.ltwh = ltwh
         self.sleep_range = sleep_range
         self.move_duration_range = move_duration_range
@@ -62,6 +62,11 @@ class Mouse:
         Clicks within the provided coordinates. If width and height are
         both 0, then this function will click in the exact same location
         every time.
+
+        Args:
+            move_away (bool): Whether to move the mouse cursor a short
+                              distance away from the coordinates that
+                              were just clicked on, default is False.
 
         """
         self.move_to()
@@ -79,10 +84,13 @@ class Mouse:
         Bezier curves to make mouse movement appear more human-like.
 
         """
-        xmin, ymin, xmax, ymax = self.ltwh
+        left, top, width, height = self.ltwh
 
-        x_coord = rand.randint(xmin, (xmin + xmax))
-        y_coord = rand.randint(ymin, (ymin + ymax))
+        # hc.move uses a (x1, x2, y1, y2) coordinate format instead of a
+        #   (left, top, width, height) format.
+        # x2 and y2 are obtained by adding width to left and height to top.
+        x_coord = rand.randint(left, (left + width))
+        y_coord = rand.randint(top, (top + height))
 
         hc.move((x_coord, y_coord), self.move_duration())
         return True
@@ -98,22 +106,24 @@ class Mouse:
         the minimum X distance and whichever of the two values is higher
         will be used as the maximum X distance. Same for top/height.
 
-
         """
         left, top, width, height = self.ltwh
         (x_position, y_position) = pag.position()
 
+        # Get min and max values based on the provided ltwh coordinates.
         x_min = min(left, width)
         x_max = max(left, width)
         y_min = min(top, height)
         y_max = max(top, height)
 
+        # Get a random distance to move based on min and max values.
         x_distance = rand.randint(x_min, x_max)
         y_distance = rand.randint(y_min, y_max)
 
-        # Roll for a chance to reverse the direction the mouse moves in.
         y_destination = y_position + y_distance
         x_destination = x_position + x_distance
+
+        # Roll for a chance to reverse the direction the mouse moves in.
         if (rand.randint(1, 2)) == 2:
             x_destination = x_position - x_distance
         if (rand.randint(1, 2)) == 2:
@@ -132,8 +142,7 @@ class Mouse:
 
         """
         move_durmin, move_durmax = self.move_duration_range
-        move_duration_var = misc.rand_seconds(rmin=move_durmin,
-                                              rmax=move_durmax)
+        move_duration_var = misc.rand_seconds(rmin=move_durmin, rmax=move_durmax)
         return move_duration_var
 
     def click(self, hold=False):
@@ -145,22 +154,22 @@ class Mouse:
             hold (bool): Whether to hold down the mouse button rather
                          than just clicking it.
                          Uses self.action_duration_range to determine
-                         min and max duration.
+                         the minimum and maximum duration to hold down
+                         the mouse button.
 
         """
-        misc.sleep_rand(rmin=self.sleep_range[0],
-                        rmax=self.sleep_range[1])
+        # Random sleep before click.
+        misc.sleep_rand(rmin=self.sleep_range[0], rmax=self.sleep_range[1])
 
         if hold is True:
             duration = misc.rand_seconds(rmin=self.action_duration_range[0],
                                          rmax=self.action_duration_range[1])
-            pag.click(button=self.button,
-                      duration=duration)
+            pag.click(button=self.button, duration=duration)
         else:
             pag.click(button=self.button)
 
-        misc.sleep_rand(rmin=self.sleep_range[2],
-                        rmax=self.sleep_range[3])
+        # Random sleep after click.
+        misc.sleep_rand(rmin=self.sleep_range[2], rmax=self.sleep_range[3])
         return True
 
 
@@ -169,41 +178,43 @@ class Keyboard:
     Manipulates the keyboard.
 
     Args:
-        sleep_range (tuple): A 4-member tuple containing the minimum
-                             and maximum number of miliseconds to wait
+        sleep_range (tuple): A 4-tuple containing the minimum and
+                             maximum number of miliseconds to wait
                              before performing the action, and the
                              minimum and maximum number of miliseconds
                              to wait after performing the action,
                              default is (0, 500, 0, 500).
-        action_duration_range (tuple): A 2-member tuple containing the
+        action_duration_range (tuple): A 2-tuple containing the
                                        minimum and maximum number of
                                        miliseconds during which the
                                        action will be performed, such as
-                                       holding down the mouse button,
-                                       default is (1, 100).
-        log (bool): Whether to log keystrokes at DEBUG level. This is
-                    turned off for user credentials.
+                                       holding down a key, default is
+                                       (1, 100).
+        log_keys (bool): Whether to log keystrokes at DEBUG level. This
+                         is always set to False when entering user
+                         credentials, default is True.
 
     """
     def __init__(self,
                  sleep_range=(0, 500, 0, 500),
                  action_duration_range=(1, 100),
-                 log=True):
+                 log_keys=True):
+
         self.sleep_range = sleep_range
         self.action_duration_range = action_duration_range
-        self.log = log
+        self.log = log_keys
 
     def typewriter(self, message):
         """
         Types out the specified message with a randomized delay between
-        key presses.
+        each key press.
 
         Args:
             message (str): The message to type.
 
         """
         self.sleep_range = (0, 20, 0, 20)
-        self.action_duration_range = (1, 100)
+        self.action_duration_range = (1, 50)
         for key in message:
             self.keypress(key)
         return True
