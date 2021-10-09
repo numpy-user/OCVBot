300a48d9a05a7a56866be0ff16b6c68ac2668cb8
takelley1 <hxyz@protonmail.com>
2021-10-09 15:02:58 -0400

    Rewrite osrs.sh

--- tools/osrs.sh
+++ tools/osrs.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
 # Downloads and runs the OSRS client on Linux.
-# Requires p7zip, wget, and java.
+# Requires p7zip, wget, and java-11-openjdk.
 
-# Your OpenJDK installation path may be different. To find out your path,
-#   run `which java`.
-java_path="/usr/bin/java"
+java_path="/usr/lib/jvm/java-11-openjdk/bin/java"
 
 # These varaiables are unlikely to change.
 server="http://oldschool.runescape.com/jav_config.ws"
@@ -13,50 +11,61 @@ dmg_url="https://www.runescape.com/downloads/OldSchool.dmg"
 
 # ------------------------------------------------------------------------
 
-set -u
+set -euo pipefail
 
-# Check for required packages.
-for i in wget 7z java; do
-  if [[ -z $(which "${i}") ]]; then
-    printf "%s\n" "Missing binary ${i}!"
-    exit 1
-  fi
-done
+print() {
+    printf \
+        "\n%s %s\n\n" \
+        "[$(date +%F_%T)]" \
+        "INFO: $* ####################################################################################################"
+}
+
+if ! hash wget 2>/dev/null; then echo "wget missing!" && exit 1; fi
+if ! hash 7z 2>/dev/null; then echo "7z missing!" && exit 1; fi
+if ! hash java 2>/dev/null; then echo "java missing!" && exit 1; fi
+
+cleanup() {
+    print "Cleaning up"
+    rm -rf -- \
+        /tmp/osrs*dmg \
+        "${HOME}/random.dat" \
+        "${HOME}/jagex_cl_oldschool_LIVE.dat" \
+        "${HOME}/jagexappletviewer.preferences"
+
+    if [[ -d "${HOME}/jagexcache" ]]; then
+        mv -- "${HOME}/jagexcache" "${HOME}/.jagexcache"
+    fi
+}
 
 # Clean up when killed.
-trap 'rm -rf -- "/tmp/osrs.dmg" "/tmp/osrs-dmg" "$HOME/random.dat" "$HOME/jagex_cl_oldschool_LIVE.dat" "$HOME/jagexappletviewer.preferences" &>/dev/null ; mv -- "$HOME/jagexcache" "$HOME/.jagexcache" &>/dev/null' EXIT
+trap cleanup EXIT
 
 # Un-hide the cache directory if it exists.
-mv "$HOME/.jagexcache" "$HOME/jagexcache" &>/dev/null
+if [[ -d "${HOME}/jagexcache" ]]; then
+    mv -v -- "$HOME/jagexcache" "$HOME/.jagexcache"
+fi
 
 # Pull the latest client version.
-wget "${dmg_url}" --output-document="/tmp/osrs.dmg" --no-check-certificate &>/dev/null
+print "Pulling latest image"
+wget --no-check-certificate --no-verbose "${dmg_url}" --output-document="/tmp/osrs.dmg"
 
 # Extract the JAR file from the DMG.
-7z x -y "/tmp/osrs.dmg" -o"/tmp/osrs-dmg/" &>/dev/null
+print "Extracting image"
+7z x -y "/tmp/osrs.dmg" -o"/tmp/osrs-dmg/"
 
 # Change working directory.
-cd '/tmp/osrs-dmg/Old School RuneScape/Old School RuneScape.app/Contents/Java' || exit 1
+print "Entering image directory"
+cd "/tmp/osrs-dmg/Old School RuneScape/Old School RuneScape.app/Contents/Java"
 
 # Launch the OSRS client JAR file.
-"${java_path}" -Djava.class.path=./jagexappletviewer.jar \
-               -Dsun.java2d.nodraw=true                  \
-               -Dcom.jagex.config=$server                \
-               -Xmx512m -Xss2m                           \
-               -XX:CompileThreshold=1500                 \
-               -XX:+UseConcMarkSweepGC                   \
-               jagexappletviewer "." &>/dev/null
-
-# Clean up when exited.
-rm -rf -- "/tmp/osrs.dmg" \
-          "/tmp/osrs-dmg" \
-          "$HOME/random.dat" \
-          "$HOME/jagex_cl_oldschool_LIVE.dat" \
-          "$HOME/jagexappletviewer.preferences" \
-          &>/dev/null
-
-mv -- "$HOME/jagexcache" \
-      "$HOME/.jagexcache" \
-      &>/dev/null
+print "Launching Java applet"
+"${java_path}" \
+    -Djava.class.path=./jagexappletviewer.jar \
+    -Dsun.java2d.nodraw=true \
+    -Dcom.jagex.config="${server}" \
+    -Xmx512m \
+    -Xss2m \
+    -XX:CompileThreshold=1500 \
+    jagexappletviewer "."
 
 exit 0
