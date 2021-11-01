39a26d05b72426a0416f5f7a744277ec5c533aa8
takelley1 <hxyz@protonmail.com>
2021-10-31 20:47:14 -0400

    tools: rewrite censor_screen.sh to use getopts

--- tools/censor_screen.sh
+++ tools/censor_screen.sh
@@ -1,53 +1,85 @@
 #!/usr/bin/env bash
-#
-# Censors screen elements. This is used for creating screenshots for testing
-#   that are extremely small.
-#
-# This script can be run multiple times on the same image to censor multiple
-#   screen elements.
-#
-# Example:
-#   `censor_screen.sh game image_01.png` = Censor the game screen.
-#   `censor_screen.sh inv image_01.png`  = Additionally censor the side stones and inventory.
 
-set -euo pipefail
+usage() {
+    cat <<EOF
+
+./censor_screen.sh [OPTIONS] {FILE}
+
+Censors OSRS screen elements. This is used for creating screenshots for testing
+  that are extremely small.  A backup.png file is created automatically.
+
+  -g   Censor the game screen.
+  -c   Censor the chat menu.
+  -i   Censor the side stones and inventory.
+  -m   Censor the action orbs and minimap.
+  -h   Print this help message.
+
+Examples:
+  censor_screen.sh -gc image_01.png   = Censor the game screen and chat menu.
+  censor_screen.sh -i image_02.png    = Censor the side stones and inventory.
+
+EOF
+    exit 1
+}
 
 if ! hash 2>/dev/null convert; then echo "Missing ImageMagick!" && exit 1; fi
 if ! hash 2>/dev/null pngcrush; then echo "Missing pngcrush!" && exit 1; fi
 
-cp "${2}" "backup.png"
+game=0
+chat=0
+inv=0
+map=0
+while getopts ":gcimh" opt; do
+    case "${opt}" in
+    g) game=1 ;;
+    c) chat=1 ;;
+    i) inv=1 ;;
+    m) map=1 ;;
+    h) usage ;;
+    *) usage ;;
+    esac
+done
+shift $((OPTIND - 1))
 
-# Censors the primary game screen.
-if [[ "${1}" == "game" ]]; then
-    convert "${2}" \
-        -fill black \
-        -draw "rectangle 0 0 516 340" \
-        "${2}"
-
-# Censors the chat menu.
-elif [[ "${1}" == "chat" ]]; then
-    convert "${2}" \
-        -fill black \
-        -draw "rectangle 0 340 524 503" \
-        "${2}"
-
-# Censors the side stones and inventory.
-elif [[ "${1}" == "inv" ]]; then
-    convert "${2}" \
-        -fill black \
-        -draw "rectangle 516 169 765 503" \
-        "${2}"
-
-# Censors the compass, status orbs, and minimap.
-elif [[ "${1}" == "map" ]]; then
-    convert "${2}" \
-        -fill black \
-        -draw "rectangle 517 0 765 169" \
-        "${2}"
-
-else
-    printf "%s\n" "Incorrect options!"
-    exit 1
+if [[ -z "${1}" ]]; then
+    usage
 fi
 
-pngcrush -ow "${2}"
+set -euo pipefail
+IFS=$'\n\t'
+
+cp -f -- "${1}" "backup.png"
+
+# Censors the primary game screen.
+if [[ "${game}" -eq 1 ]]; then
+    convert "${1}" \
+        -fill black \
+        -draw "rectangle 0 0 516 340" \
+        "${1}"
+fi
+
+# Censors the chat menu.
+if [[ "${chat}" -eq 1 ]]; then
+    convert "${1}" \
+        -fill black \
+        -draw "rectangle 0 340 524 503" \
+        "${1}"
+fi
+
+# Censors the side stones and inventory.
+if [[ "${inv}" -eq 1 ]]; then
+    convert "${1}" \
+        -fill black \
+        -draw "rectangle 516 169 765 503" \
+        "${1}"
+fi
+
+# Censors the compass, status orbs, and minimap.
+if [[ "${map}" -eq 1 ]]; then
+    convert "${1}" \
+        -fill black \
+        -draw "rectangle 517 0 765 169" \
+        "${1}"
+fi
+
+pngcrush -ow "${1}"
