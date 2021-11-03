8c87b6f3815e131c00609d300da4cd0b6fdf4877
takelley1 <hxyz@protonmail.com>
2021-11-03 19:40:55 -0400

    tests: add count_needles() test

---
+++ tests/test_vision.py
@@ -0,0 +1,36 @@
+# coding=UTF-8
+"""
+Unit tests for the vision.py module.
+
+Linux only. Requires feh.
+
+"""
+import logging as log
+import os
+
+import psutil
+import pytest
+
+import common
+
+image_directory = (os.path.dirname(__file__)) + "/test_vision/"
+
+log.basicConfig(
+    format="%(asctime)s %(filename)s.%(funcName)s - %(message)s", level="DEBUG"
+)
+
+# Provide an image for the client to orient itself. Currently any imports
+#   from ocvbot require an image to match first, or they will fail.
+common.feh("orient", "pass", "01", ((os.path.dirname(__file__)) + "/test_vision/"))
+from ocvbot import vision as vis
+
+# COUNT_NEEDLES -----------------------------------------------------------------------------------
+
+count_needles_pass_params = (("01"),)
+
+@pytest.mark.parametrize("params", count_needles_pass_params)
+def test_count_needles_pass(params) -> None:
+    test_number = params
+    common.feh("count_needles", "pass", test_number, image_directory)
+    result = vis.Vision(region=vis.inv, needle="./needles/items/iron-bar.png").count_needles()
+    assert result == 27
