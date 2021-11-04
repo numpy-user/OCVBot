28562cb3b11ccf788bc9aafa2a34ee3038847eea
takelley1 <hxyz@protonmail.com>
2021-11-03 22:14:25 -0400

    tools: censor side stones and inv separately in censor_screen.sh

--- tools/censor_screen.sh
+++ tools/censor_screen.sh
@@ -11,9 +11,10 @@ Censors OSRS screen elements. This is used for creating screenshots for testing
   -b   Censor the primary bank window, excluding tabs and settings.
   -g   Censor the game screen.
   -c   Censor the chat menu.
-  -i   Censor the side stones and inventory.
+  -i   Censor the inventory.
   -m   Censor the action orbs and minimap.
-  -s   Censor the bank settings at the bottom of the bank window.
+  -s   Censor the side stones and columns around the inventory.
+  -S   Censor the bank settings at the bottom of the bank window.
   -t   Censor the bank tabs at the top of the bank window.
 
   -h   Print this help message.
@@ -30,20 +31,22 @@ if ! hash 2>/dev/null convert; then echo "Missing ImageMagick!" && exit 1; fi
 if ! hash 2>/dev/null pngcrush; then echo "Missing pngcrush!" && exit 1; fi
 
 bank_window=0
-game=0
-chat=0
-inv=0
-map=0
+game_screen=0
+chat_menu=0
+inventory=0
+minimap=0
+side_stones=0
 bank_settings=0
 bank_tabs=0
-while getopts ":bgcimhst" opt; do
+while getopts ":bgcimhsSt" opt; do
     case "${opt}" in
     b) bank_window=1 ;;
-    g) game=1 ;;
-    c) chat=1 ;;
-    i) inv=1 ;;
-    m) map=1 ;;
-    s) bank_settings=1 ;;
+    g) game_screen=1 ;;
+    c) chat_menu=1 ;;
+    i) inventory=1 ;;
+    m) minimap=1 ;;
+    s) side_stones=1 ;;
+    S) bank_settings=1 ;;
     t) bank_tabs=1 ;;
     h) usage ;;
     *) usage ;;
@@ -60,39 +63,7 @@ IFS=$'\n\t'
 
 cp -f -- "${1}" "backup.png"
 
-# Censors the primary game screen.
-if [[ "${game}" -eq 1 ]]; then
-    convert "${1}" \
-        -fill black \
-        -draw "rectangle 0 0 516 340" \
-        "${1}"
-fi
-
-# Censors the chat menu.
-if [[ "${chat}" -eq 1 ]]; then
-    convert "${1}" \
-        -fill black \
-        -draw "rectangle 0 340 524 503" \
-        "${1}"
-fi
-
-# Censors the side stones and inventory.
-if [[ "${inv}" -eq 1 ]]; then
-    convert "${1}" \
-        -fill black \
-        -draw "rectangle 516 169 765 503" \
-        "${1}"
-fi
-
-# Censors the compass, status orbs, and minimap.
-if [[ "${map}" -eq 1 ]]; then
-    convert "${1}" \
-        -fill black \
-        -draw "rectangle 517 0 765 169" \
-        "${1}"
-fi
-
-# Censors the primary bank window, excluding tabs and settings.
+# -b = Censors the primary bank window, excluding tabs and settings.
 if [[ "${bank_window}" -eq 1 ]]; then
     convert "${1}" \
         -fill black \
@@ -100,7 +71,54 @@ if [[ "${bank_window}" -eq 1 ]]; then
         "${1}"
 fi
 
-# Censors the bank settings.
+# -c = Censors the chat menu.
+if [[ "${chat_menu}" -eq 1 ]]; then
+    convert "${1}" \
+        -fill black \
+        -draw "rectangle 0 340 524 503" \
+        "${1}"
+fi
+
+# -g = Censors the primary game screen.
+if [[ "${game_screen}" -eq 1 ]]; then
+    convert "${1}" \
+        -fill black \
+        -draw "rectangle 0 0 516 340" \
+        "${1}"
+fi
+
+# -i = Censors and inventory.
+if [[ "${inventory}" -eq 1 ]]; then
+    convert "${1}" \
+        -fill black \
+        -draw "rectangle 550 205 734 467" \
+        "${1}"
+fi
+
+# -m = Censors the compass, status orbs, and minimap.
+if [[ "${minimap}" -eq 1 ]]; then
+    convert "${1}" \
+        -fill black \
+        -draw "rectangle 517 0 765 169" \
+        "${1}"
+fi
+
+# -s = Censors the side stones and columns.
+if [[ "${side_stones}" -eq 1 ]]; then
+    convert "${1}" \
+        -fill black \
+        -draw "rectangle 517 170 765 205" \
+        -draw "rectangle 517 467 765 503" \
+        -draw "rectangle 517 205 550 467" \
+        -draw "rectangle 734 205 765 467" \
+        "${1}"
+    # -draw "rectangle 517 170 765 205" \ = Top side stones
+    # -draw "rectangle 517 467 765 503" \ = Bottom side stones
+    # -draw "rectangle 517 205 550 467" \ = Left column
+    # -draw "rectangle 734 205 765 467" \ = Right column
+fi
+
+# -S = Censors the bank settings.
 if [[ "${bank_settings}" -eq 1 ]]; then
     convert "${1}" \
         -fill black \
@@ -108,7 +126,7 @@ if [[ "${bank_settings}" -eq 1 ]]; then
         "${1}"
 fi
 
-# Censors the bank tabs.
+# -t = Censors the bank tabs.
 if [[ "${bank_tabs}" -eq 1 ]]; then
     convert "${1}" \
         -fill black \
