600624f43f3253cd499f1fc6936f2dd669e214fd
T. Austin Kelley <10100000101@protonmail.com>
2021-11-05 10:09:45 -0400

    interface: add invert_match arg to enable_button()

--- ocvbot/interface.py
+++ ocvbot/interface.py
@@ -15,6 +15,7 @@ def enable_button(
     button_enabled: str,
     button_enabled_region: tuple[int, int, int, int],
     conf: float = 0.95,
+    invert_match: bool = False,
 ):
     """
     Enables a button in the interface. Tries multiple times to ensure the
@@ -22,14 +23,14 @@ def enable_button(
     different from its "disabled" state.
 
     More generically, this function can also be used to confirm certain actions
-    have taken place (see second example).
+    have taken place (see example #2).
 
     Args:
         button_disabled (str): Filepath to an image of the disabled version
                                of the button.
         button_disabled_region (tuple): Vision region to use to search for the
                                         button_disabled image (e.g. `vis.inv` or
-                                        `vis.side_stones` or `vis.game_screen`.)
+                                        `vis.side_stones` or `vis.game_screen`)
         button_enabled (str): Filepath to an image of the enabled version of
                               the button.
         button_enabled_region (tuple): Vision region to use to search for the
@@ -37,6 +38,9 @@ def enable_button(
         conf (float): Confidence required to match button images. See the `conf`
                       arg in the docstring of the `Vision` class for more info.
                       Default is `0.95`.
+        invert_match (bool): Setting this to True will cause the function to
+                             check for the absence of button_enabled, instead
+                             of its presence (see example #3). Default is False.
 
     Examples:
         Open a side stone:
@@ -47,6 +51,11 @@ def enable_button(
         Logout of the game client:
             enable_button("./needles/buttons/logout.png", vis.inv,
                           "./needles/login-menu/orient.png", vis.game_screen)
+        Close the bank window. Since the "close" button disappears after
+        clicking on it, we must invert the match:
+            enable_button("./needles/bank/close.png", vis.game_screen,
+                          "./needles/bank/close.png", vis.game_screen,
+                          0.95, True)
 
     Returns:
         Returns True if the button was enabled or was already enabled.
@@ -62,9 +71,14 @@ def enable_button(
         button_enabled_needle = vis.Vision(
             region=button_enabled_region, needle=button_enabled, loop_num=1, conf=conf
         ).wait_for_needle()
-        if button_enabled_needle is True:
-            log.debug("Button %s is enabled", button_enabled)
-            return True
+        if invert_match is False:
+            if button_enabled_needle is True:
+                log.debug("Button %s is enabled", button_enabled)
+                return True
+        elif invert_match is True:
+            if button_enabled_needle is False:
+                log.debug("Button %s is enabled (invert_match)", button_enabled)
+                return True
 
         log.debug("Attempting to enable button %s", button_enabled)
         # Move mouse out of the way after clicking so the function can
