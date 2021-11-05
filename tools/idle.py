93c352cc5ad3ce216b9d4bf3d8667807bf10aaf6
T. Austin Kelley <10100000101@protonmail.com>
2021-11-05 16:18:00 -0400

    tools: rewrite idle.py

--- tools/idle.py
+++ tools/idle.py
@@ -1,53 +1,46 @@
 # coding=UTF-8
 """
-Keeps the OSRS client logged in.
+Keeps the OSRS client logged in by randomly pressing an arrow key every
+few minutes.
 
 """
 import logging as log
-import os
 import pathlib
 import random as rand
 import sys
-import yaml
 
 # Ensure ocvbot files are added to sys.path.
 SCRIPTPATH = str(pathlib.Path(__file__).parent.parent.absolute())
 sys.path.insert(1, SCRIPTPATH)
 
-from ocvbot import inputs, misc, vision as vis
-
-# Focus the client.
-inputs.Mouse(region=vis.chat_menu).click_coord(move_away=True)
-
-with open("../ocvbot/config.yaml", encoding="utf-8") as config:
-    config = yaml.safe_load(config)
+from ocvbot import inputs
+from ocvbot import misc
+from ocvbot import vision as vis
 
 
-def kill_script():
-    """
-    Used to manually terminate the primary thread of execution.
+main() -> None:
+    # Focus the client by clicking a random spot on the chat menu.
+    inputs.Mouse(region=vis.chat_menu).click_coord()
 
-    """
-    # TODO: Replace this with psutil.kill().
-    os.system("pkill -f main.py")
+    # Every 100-299 seconds, hit an arrow key to move the client's camera.
+    # Auto-logout occurs after 5 minutes (300 seconds) of inactivity.
+    while True:
+        # Units for sleep_rand() are in miliseconds.
+        # 5 min = 300000 miliseconds.
+        misc.sleep_rand(100000, 299000)
+        roll = rand.randint(1, 4)
+        
+        if roll == 1:
+            key = "left"
+        elif roll == 2:
+            key = "right"
+        elif roll == 3:
+            key = "up"
+        else:
+            key = "down"
 
+        log.info("Pressing key %s to remain logged in", key)
+        inputs.Keyboard().keypress(key)
 
-# This requires sudo privileges, so it's optional.
-if config["main"]["keyboard_kill"] is True:
-    import keyboard
-
-    keyboard.add_hotkey(config["main"]["kill_hotkey"], kill_script)
-
-while True:
-    # Every 3-5 minutes, hit an arrow key to move the client's camera.
-    misc.sleep_rand(180000, 299000)
-    key = rand.randint(1, 4)
-    log.info("Hitting arrow key")
-    if key == 1:
-        inputs.Keyboard().keypress("left")
-    elif key == 2:
-        inputs.Keyboard().keypress("right")
-    elif key == 3:
-        inputs.Keyboard().keypress("up")
-    elif key == 4:
-        inputs.Keyboard().keypress("down")
+if __name__ == "__main__":
+    main()
