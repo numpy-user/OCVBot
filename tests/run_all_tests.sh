0307a2eddaa516a63e298cb0d6e394b759d0aca6
takelley1 <hxyz@protonmail.com>
2021-11-06 13:58:53 -0400

    tests: add run_all_tests.sh script

---
+++ tests/run_all_tests.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+#
+# Script for running the entire OCVBot test suite.
+# Automatically temporarily sets the config.yaml file to use the correct values.
+
+set -euo pipefail
+
+# Locate the main config file.
+readonly script_path="$(realpath "${0}")"
+readonly script_dir="$(dirname "${script_path}")"
+readonly config_file="${script_dir}/../ocvbot/config.yaml"
+
+readonly current_dir="$(pwd)"
+
+readonly config_file_backup="$(mktemp)"
+
+cleanup() {
+    # Restore the original config file.
+    mv -- "${config_file_backup}" "${config_file}"
+    cd "${current_dir}" || exit 1
+}
+trap cleanup EXIT
+
+# Backup the original config file before it's edited.
+cp -f -- "${config_file}" "${config_file_backup}"
+
+# Edit the config file.
+sed -i \
+    's/ctrl_click_run:.*/ctrl_click_run: False/' \
+    "${config_file}"
+
+# Run the test suite.
+cd "${script_dir}" || exit 1
+source "${script_dir}/../ocvbot_venv/bin/activate"
+pytest .
+
+exit 0
