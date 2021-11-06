d815f266bd2938bc5ed8ee7469f95b6575dd5f40
takelley1 <hxyz@protonmail.com>
2021-11-06 15:34:56 -0400

    test_main: add tests for alchemist()

--- tests/test_main.py
+++ tests/test_main.py
@@ -50,3 +50,35 @@ def test_smith_pass(params) -> None:
     result = main.smith(bar=bar_type, item=item, location=location, loops=1)
     assert result is True
     common.kill_feh()
+
+
+# ALCHEMIST ---------------------------------------------------------------------------------------
+
+alchemist_pass_params = (
+    ("bank-note", "01"),  # Item is in top left.
+    ("bank-note", "02"),  # Item is at bottom.
+    ("bank-note", "03"),  # Item is at bottom, side stone must be opened.
+)
+
+alchemist_fail_params = (
+    ("bank-note", "01"),  # Item on the right side of inventory.
+    ("bank-note", "02"),  # Item on the right side of inventory, near center.
+)
+
+
+@pytest.mark.parametrize("params", alchemist_pass_params)
+def test_alchemist_pass(params) -> None:
+    alch_item_type, test_number = params
+    common.feh("alchemist", "pass", test_number, image_directory)
+    result = main.alchemist(alch_item_type=alch_item_type, loops=1)
+    assert result is None
+    common.kill_feh()
+
+
+@pytest.mark.parametrize("params", alchemist_fail_params)
+def test_alchemist_fail(params) -> None:
+    alch_item_type, test_number = params
+    common.feh("alchemist", "fail", test_number, image_directory)
+    with pytest.raises(Exception, match="Could not find target"):
+        main.alchemist(alch_item_type=alch_item_type, loops=1)
+    common.kill_feh()
