c3af547e35c754916b01e984f374c2e30ed60d9a
T. Austin Kelley <10100000101@protonmail.com>
2021-11-12 14:06:02 -0500

    init: remove non-useful screenshot cleanup

--- ocvbot/__init__.py
+++ ocvbot/__init__.py
@@ -32,8 +32,3 @@ log_level = config["main"]["log_level"]
 log.basicConfig(
     format="%(asctime)s %(filename)s.%(funcName)s - %(message)s", level=log_level
 )
-
-# TODO: Find a better way to do this.
-# Clean up left over screenshots from previous runs.
-if os.name == "posix":
-    os.system("rm .screenshot2*.png >/dev/null 2>&1")
